a75a275023c754db45d09c8ff455b89b6906a958
AishMishra001 <aishlunatic001@gmail.com>
2024-09-06 22:36:54 +0530

    generating mnemonics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { generateMnemonic } from 'bip39';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [mnemonic , setMnemonic] = useState("") ; 
 
   return (
     <>
-     
+     <button onClick={ async function(){
+      const mn = await generateMnemonic(); 
+      setMnemonic(mn) ; 
+     }}>
+        Create Seed Phase 
+     </button>
+     <input type="text" value={mnemonic}></input>
     </>
   )
 }
+console.log(mnemonic) ; 
 
 export default App
